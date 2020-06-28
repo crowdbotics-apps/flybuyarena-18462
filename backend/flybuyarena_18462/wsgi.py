cd74cc4c14962fb1e77411e47200c18d942995ed
Crowdbotics <team@crowdbotics.com>
2020-06-28 09:56:33 +0000

    Update models

--- backend/flybuyarena_18462/wsgi.py
+++ backend/flybuyarena_18462/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flybuyarena_18462.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flybuyarena_18462.settings")
 
 application = get_wsgi_application()
